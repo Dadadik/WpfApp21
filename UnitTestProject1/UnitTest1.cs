3ff1b52281d7c599c4628456b6130181b8fb7374
Dadadik <sundu.dunduk@mail.ru>
2024-03-19 01:26:24 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WpfApp21;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckNumberX_EnterMinus6_AnswerDoNotBelong()
+        {
+            double x = -6;
+            string excpected = "не принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        [TestMethod]
+        public void CheckNumberX_EnterMinus5_AnswerBelong()
+        {
+            double x = -5;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        [TestMethod]
+        public void CheckNumberX_Enter2_AnswerBelong()
+        {
+            double x = 2;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        public void CheckNumberX_Enter5_AnswerBelong()
+        {
+            double x = 5;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        [TestMethod]
+        public void CheckNumberX_Enter7_AnswerDoNotBelong()
+        {
+            double x = 7;
+            string excpected = "не принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        [TestMethod]
+        public void CheckNumberX_Enter12_AnswerBelong()
+        {
+            double x = 12;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        [TestMethod]
+        public void CheckNumberX_Enter15_AnswerBelong()
+        {
+            double x = 15;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        public void CheckNumberX_Enter23_AnswerBelong()
+        {
+            double x = 23;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+        public void CheckNumberX_Enter25_AnswerDoNotBelong()
+        {
+            double x = 25;
+            string excpected = "принадлежит";
+            string actual = MainWindow.CheckNumberX(x);
+            Assert.AreEqual(excpected, actual);
+        }
+    }
+}
---
+++ WpfApp21/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp21
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BtnOKClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+              
+                double x = Convert.ToDouble(TbNumberX.Text);
+                TextBlockAnswer.Text = $" Точка {x:n2} {CheckNumberX(x)}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+   public static string CheckNumberX(double x)
+        {
+            if (x >= -5 && x <= 5)
+                return "принадлежит";
+
+            else if (x >= 12 && x <= 23)
+                return "принадлежит";
+            else
+                return "не принадлежит";
+        }
+    }
+}
